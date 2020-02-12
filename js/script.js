16f2691410f762bd45caa877f85bac60272555d2
Karolina Jurczak <inka_c@tlen.pl>
2020-02-12 12:52:24 +0100

    Add rwd elements to page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 function toggleMenu(visible) {
   document.querySelector('.navigation').classList.toggle('show', visible);
+  document.querySelector('.manager').classList.toggle('show', visible);
+  document.querySelector('.section__topbar-at-sidebar').classList.toggle('show', visible);
 }
 
 document.querySelector('.logo-link').addEventListener('click', function(e) {
