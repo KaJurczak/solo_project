d46965563b2ba5b005e333d71873f4c3d9ac5f8d
Karolina Jurczak <inka_c@tlen.pl>
2020-02-17 12:41:17 +0100

    Add first modal

--- js/script.js
+++ js/script.js
@@ -8,3 +8,41 @@ document.querySelector('.logo-link').addEventListener('click', function(e) {
   e.preventDefault();
   toggleMenu();
 });
+
+/* remove class "show" from modal */
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show');
+}
+
+/* remove class "show" after clicking at button  */
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+/* remove class "show" after clicking at overlay  */
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal();
+  }
+});
+
+/* remove class "show" after clicking at escape  */
+document.addEventListener('keyup', function(e) {
+  if(e.keyCode === 27) {
+    closeModal();
+  }
+});
+
+/* close all modals, than open our modal and overlay  */
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+}
+
+openModal('#myModal');
